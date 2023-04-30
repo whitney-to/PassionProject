79716887321e0bf39bfeb502d78ebe6c07e3d37b
whitney-to <whitney012923@gmail.com>
2023-04-29 20:31:59 -0400

    Initial version of Tourista generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/tourista/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.tourista;
+
+import com.tourista.TouristaApp;
+import com.tourista.config.AsyncSyncConfiguration;
+import com.tourista.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { TouristaApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
